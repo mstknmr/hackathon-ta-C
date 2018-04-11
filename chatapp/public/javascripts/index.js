7cfd6151fcfbe776b896d91b46165f0853b362a9
mstknmr <15nc012@ms.dendai.ac.jp>
2018-04-11 21:14:37 +0900

    committed in 2018/04/11 21:14:37

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -5,7 +5,6 @@ function enter() {
     // 入力されたユーザ名を取得する
     const userName =$('#userName').val();
     // ユーザ名が未入力でないかチェックする
-    const first = userName.slice(0,1);
 
     if(userName.trim() === '')//一文字目が空欄の場合を弾く
     {
