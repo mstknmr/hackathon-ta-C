2af21f535e19ef162fad2b1c390abdb2b1748cf6
104aki <csts15019@g.nihon-u.ac.jp>
2018-04-11 21:16:28 +0900

    committed in 2018/04/11 21:16:28

--- chatapp/public/javascripts/exit.js
+++ chatapp/public/javascripts/exit.js
@@ -3,14 +3,14 @@
 // 退室メッセージをサーバに送信する
 function exit() {
     // ユーザ名取得
-    const userName = '';
+    const userName = $('#userName').val();
     // 退室メッセージイベントを送信する
-    
+    socket.emit('exit1', userName + 'さんが退出しました。')
     // 退室
     location.href = '/';
 }
 
 // サーバから受信した退室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('exit2', function (data) {
+    $('#thread').prepend('<p>' +data+ '</p>');
 });
--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -2,7 +2,9 @@
 
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
-    socket.on('', function (data) {
-        
+    socket.on('exit1', function (data) {
+
+      socket.broadcast.emit('exit2',data);
+
     });
 };
