57524979126614b34a45a361db3c22506fcffa6d
mstknmr <15nc012@ms.dendai.ac.jp>
2018-04-11 21:32:17 +0900

    committed in 2018/04/11 21:32:17

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -6,7 +6,7 @@ function enter() {
     const userName =$('#userName').val();
     // ユーザ名が未入力でないかチェックする
 
-    if(userName.trim() === '')//一文字目が空欄の場合を弾く
+    if(userName.trim() === ''||userName == '')//一文字目が空欄の場合を弾く
     {
       alert('名前が空欄です。\n再度入力してください。');
     }
