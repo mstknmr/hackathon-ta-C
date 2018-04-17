c0fb557b1f2ad572f5ed0d63ad9e15c9e7477aea
satoukeisuke <kisuka.tennis@gmail.com>
2018-04-17 18:34:12 +0900

    committed in 2018/04/17 18:34:12

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -1,16 +1,20 @@
 'use strict';
 
-// 投稿メッセージをサーバに送信する
+
 function publish() {
     // ユーザ名を取得
-    const userName = '';
+    const userName =$('#userName').val();
     // 入力されたメッセージを取得
-    const message = '';
+    const message =$('#message').val();
+    // 投稿メッセージをサーバに送信する
+    socket.emit('event1', $('#userName').val()+$('#message').val() );
     // 投稿内容を送信
-    return false;
+    //return false;
+
+
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('event2', function (data) {
+    $('#thread').prepend('<p>' + data + '</p>');
 });
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -2,7 +2,13 @@
 
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
-    socket.on('', function (data) {
-        
+    socket.on('event1', function (data) {
+      if(!data){
+        return;
+      }
+
+      console.log('クライアントの入力値：' +data);
+
+      io.sockets.emit('event2', data);
     });
 };
