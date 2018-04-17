962f2fedafef5b14b154b78061357263bb65d033
mstknmr <15nc012@ms.dendai.ac.jp>
2018-04-17 18:53:30 +0900

    committed in 2018/04/17 18:53:30

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -1,26 +1,22 @@
 'use strict';
 
 function pressEnter(){
-  $('#userName').keypress(function(e){
+  $('#userName').keypress(function(e)
+  {
     if(e.which === 13)
       {
-        const userName =$('#userName').val();
-        // ユーザ名が未入力でないかチェックする
-        //テキストボックス内においてエンターキーで名前なしでログインできる問題の対処
 
         if(userName.trim() === '')//userNameが空欄の場合を弾く
           {
             alert('名前が空欄です。\n再度入力してください。');
+            return false;
           }
         else
           {
             $('form').submit();
           }
       }
-
     });
-    return true;
-
 }
 
 // チャットルームに入室する
