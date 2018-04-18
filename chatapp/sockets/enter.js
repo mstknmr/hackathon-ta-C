bf42608414f2fd88b1cffdb28b70ba23a791e53b
nishiyuu <a15.canr@g.chuo-u.ac.jp>
2018-04-18 19:00:57 +0900

    committed in 2018/04/18 19:00:57

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -3,10 +3,10 @@
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
     socket.on('event1',function(data){
-      io.socket.emit('event2',message);
+
       if(!data){
         return;
       }
-    /*  io.socket.emit('event2',message);*/
+    io.socket.emit('event2',data);
     }
 };
