0b822c051902a70cd569fbb6a41e68697c8b547e
nishiyuu <a15.canr@g.chuo-u.ac.jp>
2018-04-18 20:29:44 +0900

    committed in 2018/04/18 20:29:44

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -1,17 +1,15 @@
 'use strict';
 
-
-function enter(){
 console.log('入室メッセージがみれるはず');
 // 入力されたユーザ名を取得する
-const userName = $('userName').val();
+const userName = $('userName1').val();
 // 入室メッセージをサーバに送信する
-io.socket.emit('event1',userName +'さんが入室しました');
-}
+io.socket.emit('enter1',userName +'さんが入室しました');
+
 
 
 
 // サーバから受信した入室メッセージを画面上に表示する
-socket.on('event2', function (data) {
+socket.on('enter2', function (data) {
     $('#thread').prepend('<p>'+ data + '</p>');
 });
--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,12 +2,12 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-    socket.on('event1',function(data){
+    socket.on('enter1',function(data){
 console.log('入室メッセージがみれるはず' + data);
       if(!data){
         return;
       }
-    socket.broadcast.emit('event2',data);
+    socket.broadcast.emit('enter2',data);
   }
 );
 };
